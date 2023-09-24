8eaf4cb37bcc2e92546b5e6216d4490287a18aae
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-25 01:43:36 +0300

    completed lab..

--- server/models.py
+++ server/models.py
@@ -1,15 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db
 
 class Newsletter(db.Model):
-    __tablename__ = 'newsletters'
-
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    body = db.Column(db.String)
-    published_at = db.Column(db.DateTime, server_default=db.func.now())
-    edited_at = db.Column(db.DateTime, onupdate=db.func.now())
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.Text, nullable=True)
 
-    def __repr__(self):
-        return f'<Newsletter {self.title}, published at {self.published_at}.>'
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
